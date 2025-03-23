ece76ab464b5130fd9adde451f908c10c6f9e55c
YourLocalMoon <yourlocalmoon7@gmail.com>
2025-03-23 12:53:38 +0200

    [feat] add lostcoast
    also add nextbot support to most games

--- src/createallprojects.bat
+++ src/createallprojects.bat
@@ -1,3 +1,3 @@
 @echo off
-devtools\bin\vpc.exe /hl2 /episodic /portal /cstrike /dod /hl1 /hl1mp /hl2mp /tf /define:SOURCESDK +everything /mksln everything.sln
+devtools\bin\vpc.exe /hl2 /episodic /lostcoast /portal /cstrike /dod /hl1 /hl1mp /hl2mp /tf /define:SOURCESDK +everything /mksln everything.sln
 pause
